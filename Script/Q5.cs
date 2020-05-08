14387b05cbd283de5be3f78af49509abc78f6b11
tonhomn <woraphat.tonhom@mail.kmutt.ac.th>
2020-05-09 04:17:26 +0700

    Quiz1_woraphat_0052

---
+++ Script/Q5.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class check : MonoBehaviour
+
+{
+
+    float x = 0;
+    float y = 0;
+    float z = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.Translate(new Vector3(x , y , z)*Time.deltaTime);
+    }
+     void OnCollisionEnter()
+    {
+        z = z * -1;
+    }
+}
